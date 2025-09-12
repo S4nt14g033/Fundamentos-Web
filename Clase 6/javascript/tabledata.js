9b9beb8c1aecb938f5348b4da368aca1325b9a6a
S4nt14g033 <nsnino@estudiante.uniajc.edu.co>
2025-09-12 13:28:54 +0000

    tabla dina

--- Clase 6/javascript/tabledata.js
+++ Clase 6/javascript/tabledata.js
@@ -2,11 +2,15 @@ const tableBodyData = [
     { name: "Lael Greer", position: "Systems Administrator", office: "London", salary: "$103,500" },
     { name: "Jonas Alexander", position: "Developer", office: "San Francisco", salary: "$86,500" },
     { name: "Shad Decker", position: "Regional Director", office: "Edinburgh", salary: "$183,000" },
+    { name: "Michael Bruce", position: "Javascript Developer", office: "Singapore", salary: "$183,000" },
+    { name: "Donna Snider", position: "Customer Support", office: "New York", salary: "$112,000" },
+    { name: "Jennifer Chang", position: "Regional Director", office: "Singapore", salary: "$357,650" },
    
 ];
 
-function loadtableData() {
-    const tableBody = document.getElementById("tableBodyData");
+function loadTableData() {
+    const tableBody = document.getElementById('tableBodyData');
+    tableBody.innerHTML = '';
 
     tableBodyData.forEach(data => {
         const row = document.createElement("tr");
@@ -20,21 +24,4 @@ function loadtableData() {
     })
 }
 
-                                        <tr>
-                                            <td>Lael Greer</td>
-                                            <td>Systems Administrator</td>
-                                            <td>London</td>
-                                            <td>$103,500</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Jonas Alexander</td>
-                                            <td>Developer</td>
-                                            <td>San Francisco</td>
-                                            <td>$86,500</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Shad Decker</td>
-                                            <td>Regional Director</td>
-                                            <td>Edinburgh</td>
-                                            <td>$183,000</td>
-                                        </tr>
+                                       
