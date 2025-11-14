8ff80fb28f76eee9e7c6f926916321454fd35226
S4nt14g0 <nickylasnino26@hotmail.com>
2025-11-14 15:34:33 -0500

    lasy class

--- Clase 14/front/js/Imagenes.js
+++ Clase 14/front/js/Imagenes.js
@@ -2,7 +2,7 @@ const uploadform = document.getElementById('uploadForm');
 const imagen = document.getElementById('imagen');
 const msg = document.getElementById('msg');
 
-uploadform.addEventListener('submit', (e) => {
+uploadform.addEventListener('submit', async (e) => {
     e.preventDefault();
     msg.textContent = '';
     
@@ -11,5 +11,27 @@ uploadform.addEventListener('submit', (e) => {
         msg.textContent = 'Por favor seleccione un archivo.';
         msg.className = 'msg err';
         return;
+    }
+
+    const formData = new FormData();
+    formData.append('image', file);
+
+    try {
+
+        const res = await fetch('http://localhost:3000/upload', {
+            method: 'POST',
+            body: formData
+        });
+
+        const data = await res.json();
+        console.log(data);
+        
+        msg.textContent = 'Archivo subido correctamente.';
+        msg.className = 'msg ok';
+
+    } catch (error) {
+        console.error(err);
+        msg.textContent = 'Error al subir la imagen.';
+        msg.className = 'msg err';
     }
 });
